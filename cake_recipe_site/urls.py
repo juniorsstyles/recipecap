48b82d46780ba0d1dafda6b1ac72f88af79e6aa0
Junior <jumiormaluleka05@gmail.com>
2024-07-31 20:34:10 +0200

    added docstrings

--- cake_recipe_site/urls.py
+++ cake_recipe_site/urls.py
@@ -6,3 +6,15 @@ urlpatterns = [
     path('', include('recipes.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  # Built-in authentication URLs
 ]
+
+"""
+Main URL configuration for the Django project.
+
+This module defines the main URL patterns for the Django project, 
+including admin, app-specific, and authentication URLs.
+
+Paths:
+    'admin/' (admin.site.urls): The Django admin interface.
+    '' (include('recipes.urls')): The URLs for the recipes app.
+    'accounts/' (include('django.contrib.auth.urls')): The built-in authentication URLs provided by Django.
+"""
